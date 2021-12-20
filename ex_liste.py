23be20ecfe15a5f444eb2fda360afbb8b7f9f059
humastefan <humastefan@gmail.com>
2021-12-20 18:27:21 +0200

    Update ex_liste.py

--- ex_liste.py
+++ ex_liste.py
@@ -4,27 +4,41 @@ varsta = [14, 23, 15, 14, 12, 41, 39]
 # Mihai are varsta de 14 ani.
 # George are varsta de 23 de ani.
 
-for i in range(len(prenume)):
-    print(f"{prenume[i]} are varsta de {varsta[i]} ani.")
+#for i in range(len(prenume)): # todo scoate ###
+#    print(f"{prenume[i]} are varsta de {varsta[i]} ani.")
 
 #b) Adăugați în liste la final, corespunzător, datele: Andreea, 34 și Ioan, 23. Tipăriți ambele liste apoi.
 prenume.append("Andreea")
 prenume.append("Ioan")
 varsta.append(34)
 varsta.append(23)
-# prenume.remove("Ana")
+
+dasas = prenume.index("Ana")
+print("Ana este la indexul", dasas, "din", prenume)
 print(prenume)
 print(varsta)
-dasas = prenume.index("Ana")
-print(dasas, "aici este Ana")
-print(prenume.pop("Ana"))
+
 # c) Ștergeți datele din ambele liste despre Ana (atenție la indici).
-if "Ana" in prenume:
-    varsta.pop(prenume.pop(prenume.index("Ana")))
-    prenume.remove("Ana")
-print(prenume, varsta)
+varsta.pop(dasas)
+prenume.pop(prenume.index("Ana"))
+print(prenume)
+print(varsta)
+
 # d) Afișați primele trei elemente din lista prenume.
-#
+print(prenume[:3])
 # e) Afișați lista prenume de la dreapta la stânga.
-#
+print(prenume[::-1])
 # f) Tipăriți elementele cu indicii 2 și 4, apoi de la 0 la 5 din ambele liste.
+print(prenume[2:5:2]) # primul 2 este primul indice printat, al doilea 2 este pasul intervalului printat
+print(varsta[2:5:2])
+print(prenume[:6]) # de la 0 la 5 inclusiv - deci fara 6
+print(varsta[:6])
+# Afisati inversul
+print(varsta)
+varsta.reverse() #similar cu linia 30
+
+lista55 = [8,7,9,10,6,5,4]
+
+# Ce va afișa comanda
+print(max(lista55)//2)
+print(sum(lista55)+max(lista55)//2)

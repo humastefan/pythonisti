432f1bc427bfca78e2d478bbf211c5033f6ae418
humastefan <humastefan@gmail.com>
2021-12-24 11:53:31 +0200

    update 211224

--- numar_inversat.py
+++ numar_inversat.py
@@ -1,6 +1,14 @@
 # Se citeşte un număr natural n. Să se tipărească numărul obţinut prin inversarea poziţiilor cifrelor pe care le ocupă numărul citit.
 # Exemplu. Dacă citim n=248, se va tipări 842.
-n = int(input("numarul de inversat este:"))
+# n = int(input("numarul de inversat este:"))
+n = 248
 inv = 0
+# print(n/10)
+# print(n//10)
+# print(n%10)
 while n !=0:
-    
+    inv = inv * 10 + n%10
+    n = n//10
+print(inv)
+
+
